c9c69e613cf4dd5d09d4ec3c76b0305a31ebcdcb
RomanQed <gbakaldin@gmail.com>
2022-04-22 22:53:55 +0300

    Improve ReflectUtil

--- src/main/java/com/github/romanqed/jeflect/ReflectUtil.java
+++ src/main/java/com/github/romanqed/jeflect/ReflectUtil.java
@@ -1,12 +1,18 @@
 package com.github.romanqed.jeflect;
 
+import com.github.romanqed.jeflect.lambdas.LambdaClass;
+import com.github.romanqed.jeflect.lambdas.MetaFactory;
+
 import java.lang.annotation.Annotation;
+import java.lang.invoke.*;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Objects;
 
 public class ReflectUtil {
-    private static final LambdaFactory FACTORY = new LambdaFactory();
+    private static final LambdaFactory LAMBDA_FACTORY = new LambdaFactory();
+    private static final MetaFactory META_FACTORY = new MetaFactory(MethodHandles.lookup());
 
     public static <T> T extractAnnotationValue(Annotation annotation, String value, Class<T> type) throws
             InvocationTargetException, IllegalAccessException, NoSuchMethodException {
@@ -28,11 +34,27 @@ public class ReflectUtil {
 
     public static <T> Lambda packMethod(Class<T> owner, Method method, T bind) throws
             InvocationTargetException, InstantiationException, IllegalAccessException {
-        return FACTORY.packMethod(owner, method, bind);
+        return LAMBDA_FACTORY.packMethod(owner, method, bind);
     }
 
     public static Lambda packMethod(Class<?> owner, Method method) throws
             InvocationTargetException, InstantiationException, IllegalAccessException {
-        return packMethod(owner, method, null);
+        return LAMBDA_FACTORY.packMethod(owner, method, null);
+    }
+
+    public <T> T packLambdaHandle(LambdaClass<T> clazz, MethodHandle handle, Object bind) throws Throwable {
+        return META_FACTORY.packLambdaHandle(clazz, handle, bind);
+    }
+
+    public <T> T packLambdaMethod(LambdaClass<T> clazz, Method method, Object bind) throws Throwable {
+        return META_FACTORY.packLambdaMethod(clazz, method, bind);
+    }
+
+    public <T> T packLambdaConstructor(LambdaClass<T> clazz, Constructor<?> constructor) throws Throwable {
+        return META_FACTORY.packLambdaConstructor(clazz, constructor);
+    }
+
+    public <T> T packLambdaMethod(LambdaClass<T> clazz, Method method) throws Throwable {
+        return META_FACTORY.packLambdaMethod(clazz, method);
     }
 }
