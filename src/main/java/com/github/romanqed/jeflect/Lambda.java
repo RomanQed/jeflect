d3837a81c370c3e3a44ce51f747d7f4acd80a767
RomanQed <gbakaldin@gmail.com>
2022-04-24 16:45:11 +0300

    Add javadocs

--- src/main/java/com/github/romanqed/jeflect/Lambda.java
+++ src/main/java/com/github/romanqed/jeflect/Lambda.java
@@ -15,6 +15,12 @@ public interface Lambda {
      */
     Object call(Object[] arguments) throws Throwable;
 
+    /**
+     * Calls the target method without parameters.
+     *
+     * @return the value that the target method will return
+     * @throws Throwable if any exception occurred during the call/preparation of the packed method call.
+     */
     default Object call() throws Throwable {
         return call(Constants.EMPTY_ARGUMENTS);
     }
