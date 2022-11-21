2785ed2f7a25e40b62c3675b093adc6baa75dd8e
RomanQed <gbakaldin@gmail.com>
2022-11-21 19:10:24 +0300

    Add javadocs for byte reflection parsers

--- src/main/java/com/github/romanqed/jeflect/parser/AsmClassFileParser.java
+++ src/main/java/com/github/romanqed/jeflect/parser/AsmClassFileParser.java
@@ -3,6 +3,9 @@ package com.github.romanqed.jeflect.parser;
 import com.github.romanqed.jeflect.ByteClass;
 import org.objectweb.asm.ClassReader;
 
+/**
+ * {@link ClassFileParser} implementation uses ASM.
+ */
 public final class AsmClassFileParser implements ClassFileParser {
     private static final int OPTIONS = ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES | ClassReader.SKIP_CODE;
 
--- src/main/java/com/github/romanqed/jeflect/parser/ClassFileParser.java
+++ src/main/java/com/github/romanqed/jeflect/parser/ClassFileParser.java
@@ -2,6 +2,16 @@ package com.github.romanqed.jeflect.parser;
 
 import com.github.romanqed.jeflect.ByteClass;
 
+/**
+ * A class representing a bytecode parser that will return the analysis results as a byte reflection instance.
+ */
 public interface ClassFileParser {
+
+    /**
+     * Parses the bytecode of the class.
+     *
+     * @param classFileBuffer buffer containing class bytecode
+     * @return {@link ByteClass} instance containing parsed class data
+     */
     ByteClass parse(byte[] classFileBuffer);
 }
