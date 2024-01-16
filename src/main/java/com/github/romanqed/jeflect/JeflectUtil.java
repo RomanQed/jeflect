c3219c70aab492cfd240327c0479a3a8e39ca8f7
RomanQed <gbakaldin@gmail.com>
2024-01-16 17:14:57 +0300

    Add enum tools

--- src/main/java/com/github/romanqed/jeflect/JeflectUtil.java
+++ src/main/java/com/github/romanqed/jeflect/JeflectUtil.java
@@ -3,6 +3,11 @@ package com.github.romanqed.jeflect;
 import com.github.romanqed.jfunc.Exceptions;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * A set of utilities for interacting with reflection.
@@ -40,5 +45,47 @@ public final class JeflectUtil {
      */
     public static <T> T extractAnnotationValue(Annotation annotation) {
         return extractAnnotationValue(annotation, "value");
+    }
+
+    /**
+     * Gets all enum values as array.
+     *
+     * @param clazz enum class
+     * @param <T>   enum type
+     * @return array contains enum values
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends Enum<T>> T[] getEnumValues(Class<T> clazz) {
+        var method = Exceptions.suppress(() -> clazz.getDeclaredMethod("values"));
+        return (T[]) Exceptions.suppress(() -> method.invoke(null));
+    }
+
+    /**
+     * Gets all enum values as stream.
+     *
+     * @param clazz enum class
+     * @param <T>   enum type
+     * @return stream contains enum values
+     */
+    public static <T extends Enum<T>> Stream<T> getEnumStream(Class<T> clazz) {
+        return Arrays.stream(getEnumValues(clazz));
+    }
+
+    /**
+     * Builds map from enum values.
+     *
+     * @param clazz    enum class
+     * @param function map function
+     * @param <T>      enum type
+     * @param <V>      value type
+     * @return map contains enum-value pairs
+     */
+    public static <T extends Enum<T>, V> Map<T, V> enumToMap(Class<T> clazz, Function<T, V> function) {
+        var values = getEnumValues(clazz);
+        var ret = new HashMap<T, V>();
+        for (var value : values) {
+            ret.put(value, function.apply(value));
+        }
+        return ret;
     }
 }
