728e98e8c7b6ed395a77aefb952c25b307760300
Roman Bakaldin <gbakaldin@gmail.com>
2024-08-30 23:59:37 +0300

    Improve primitive packing at lambda module

--- lambda/src/main/java/com/github/romanqed/jeflect/lambda/AsmUtil.java
+++ lambda/src/main/java/com/github/romanqed/jeflect/lambda/AsmUtil.java
@@ -9,91 +9,62 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Map;
 
-/**
- * A utility class containing some methods for the ASM library.
- */
 final class AsmUtil {
     // String constants
-    public static final String INIT = "<init>";
-    public static final String EMPTY_DESCRIPTOR = "()V";
+    static final String INIT = "<init>";
+    static final String EMPTY_DESCRIPTOR = "()V";
     // Type constants
-    public static final Type OBJECT = Type.getType(Object.class);
-    public static final Map<Type, Type> PRIMITIVES = getPrimitives();
-    public static final Map<Type, String> PRIMITIVE_METHODS = getPrimitiveMethods();
+    static final Type OBJECT = Type.getType(Object.class);
+    static final Map<Class<?>, Class<?>> PRIMITIVES = Map.of(
+            boolean.class, Boolean.class,
+            char.class, Character.class,
+            byte.class, Byte.class,
+            short.class, Short.class,
+            int.class, Integer.class,
+            float.class, Float.class,
+            long.class, Long.class,
+            double.class, Double.class
+    );
+    static final Map<Class<?>, String> PRIMITIVE_METHODS = Map.of(
+            boolean.class, "booleanValue",
+            char.class, "charValue",
+            byte.class, "byteValue",
+            short.class, "shortValue",
+            int.class, "intValue",
+            float.class, "floatValue",
+            long.class, "longValue",
+            double.class, "doubleValue"
+    );
 
     private AsmUtil() {
     }
 
-    private static Map<Type, Type> getPrimitives() {
-        return Map.of(
-                Type.BOOLEAN_TYPE, Type.getType(Boolean.class),
-                Type.CHAR_TYPE, Type.getType(Character.class),
-                Type.BYTE_TYPE, Type.getType(Byte.class),
-                Type.SHORT_TYPE, Type.getType(Short.class),
-                Type.INT_TYPE, Type.getType(Integer.class),
-                Type.FLOAT_TYPE, Type.getType(Float.class),
-                Type.LONG_TYPE, Type.getType(Long.class),
-                Type.DOUBLE_TYPE, Type.getType(Double.class)
+    static void castReference(MethodVisitor visitor, Class<?> clazz) {
+        if (clazz == Object.class) {
+            return;
+        }
+        if (!clazz.isPrimitive()) {
+            visitor.visitTypeInsn(Opcodes.CHECKCAST, Type.getInternalName(clazz));
+            return;
+        }
+        var wrap = Type.getInternalName(PRIMITIVES.get(clazz));
+        visitor.visitTypeInsn(Opcodes.CHECKCAST, wrap);
+        var method = PRIMITIVE_METHODS.get(clazz);
+        visitor.visitMethodInsn(
+                Opcodes.INVOKEVIRTUAL,
+                wrap,
+                method,
+                "()" + Type.getDescriptor(clazz),
+                false
         );
     }
 
-    private static Map<Type, String> getPrimitiveMethods() {
-        return Map.of(
-                Type.BOOLEAN_TYPE, "booleanValue",
-                Type.CHAR_TYPE, "charValue",
-                Type.BYTE_TYPE, "byteValue",
-                Type.SHORT_TYPE, "shortValue",
-                Type.INT_TYPE, "intValue",
-                Type.FLOAT_TYPE, "floatValue",
-                Type.LONG_TYPE, "longValue",
-                Type.DOUBLE_TYPE, "doubleValue"
-        );
-    }
-
-    /**
-     * Casts the last value on the stack to the specified type.
-     *
-     * @param visitor the visitor containing the method code
-     * @param type    the type to which the value will be cast
-     */
-    public static void castReference(MethodVisitor visitor, Type type) {
-        if (type.equals(OBJECT)) {
+    static void packPrimitive(MethodVisitor visitor, Class<?> primitive) {
+        if (!primitive.isPrimitive()) {
             return;
         }
-        var name = type.getInternalName();
-        if (name.startsWith("[")) {
-            visitor.visitTypeInsn(Opcodes.CHECKCAST, name);
-            visitor.visitTypeInsn(Opcodes.CHECKCAST, name);
-            return;
-        }
-        var wrap = PRIMITIVES.get(type);
-        if (wrap != null) {
-            visitor.visitTypeInsn(Opcodes.CHECKCAST, wrap.getInternalName());
-            var method = PRIMITIVE_METHODS.get(type);
-            visitor.visitMethodInsn(
-                    Opcodes.INVOKEVIRTUAL,
-                    wrap.getInternalName(),
-                    method,
-                    "()" + name,
-                    false
-            );
-            return;
-        }
-        visitor.visitTypeInsn(Opcodes.CHECKCAST, name);
-    }
-
-    /**
-     * Packs the last primitive on the stack.
-     *
-     * @param visitor   the visitor containing the method code
-     * @param primitive the type of primitive to be packed
-     */
-    public static void packPrimitive(MethodVisitor visitor, Type primitive) {
-        var wrap = PRIMITIVES.get(primitive);
-        if (wrap == null) {
-            return;
-        }
-        var descriptor = Type.getMethodDescriptor(wrap, primitive);
+        var wrap = Type.getType(PRIMITIVES.get(primitive));
+        var descriptor = Type.getMethodDescriptor(wrap, Type.getType(primitive));
         visitor.visitMethodInsn(Opcodes.INVOKESTATIC,
                 wrap.getInternalName(),
                 "valueOf",
@@ -101,13 +72,7 @@ final class AsmUtil {
                 false);
     }
 
-    /**
-     * Creates an empty constructor that calls the parent constructor.
-     *
-     * @param writer     {@link ClassWriter} containing the class in which the constructor will be created
-     * @param superClass class parent
-     */
-    public static void createEmptyConstructor(ClassWriter writer, String superClass) {
+    static void createEmptyConstructor(ClassWriter writer) {
         var init = writer.visitMethod(Opcodes.ACC_PUBLIC,
                 INIT,
                 EMPTY_DESCRIPTOR,
@@ -115,28 +80,13 @@ final class AsmUtil {
                 null);
         init.visitCode();
         init.visitVarInsn(Opcodes.ALOAD, 0);
-        init.visitMethodInsn(Opcodes.INVOKESPECIAL, superClass, INIT, EMPTY_DESCRIPTOR, false);
+        init.visitMethodInsn(Opcodes.INVOKESPECIAL, OBJECT.getInternalName(), INIT, EMPTY_DESCRIPTOR, false);
         init.visitInsn(Opcodes.RETURN);
         init.visitMaxs(1, 1);
         init.visitEnd();
     }
 
-    /**
-     * Creates an empty constructor that calls the parent constructor.
-     *
-     * @param writer {@link ClassWriter} containing the class in which the constructor will be created
-     */
-    public static void createEmptyConstructor(ClassWriter writer) {
-        createEmptyConstructor(writer, OBJECT.getInternalName());
-    }
-
-    /**
-     * Performs a correct call of the passed method.
-     *
-     * @param visitor visitor containing the incomplete code of the method
-     * @param method  the method that will be called
-     */
-    public static void invoke(MethodVisitor visitor, Method method) {
+    static void invoke(MethodVisitor visitor, Method method) {
         var owner = method.getDeclaringClass();
         var isInterface = owner.isInterface();
         var opcode = Modifier.isStatic(method.getModifiers()) ?
